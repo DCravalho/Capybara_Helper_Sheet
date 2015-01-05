cc19b8f01cf8bbde889b284bafc39840c29cb982
dcravalho <deoncravalho@gmail.com>
2015-01-05 11:45:25 -0800

    Common commands

--- capybara_helper_sheet.rb
+++ capybara_helper_sheet.rb
@@ -1,11 +1,20 @@
 # Navigating 
-    visit('/projects') 
+    visit('page_url') 
 
-# Click link
+# Clicking (buttons and links)
+    click_button('Submit')
+
     click_link('id_of_link')
     click_link('Link Text')
-
+    
 # Interacting with forms 
+    choose('A Radio Button')
+    
     fill_in('First Name', :with => 'John')
- 
+
+    select('Option', :from => 'Select Box')
+    unselect('Option', :from => 'Select Box')
+
+    check('checkbox')
+    uncheck('checkbox') 
 
