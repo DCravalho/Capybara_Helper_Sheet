5e2fa69b520d60d152a82705da7de62e61c3b8ed
dcravalho <deoncravalho@gmail.com>
2015-01-06 14:17:53 -0800

    Add common commands

--- capybara_helper_sheet.rb
+++ capybara_helper_sheet.rb
@@ -1,5 +1,5 @@
 # Navigating 
-    visit ('page_url')
+    visit ('login') # path to page (tacked on to end of URL, usually  defined in config file)
     visit(post_comments_path(post))
 
 # Clicking (buttons and links)
